fc4e954ba05eba5bb4185704b9d70a1c807f7015
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2026-07-05 19:28:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "1220"))
 # Channel & Group Links Configuration
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/iPopcornMoviesGroups') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Hari_Moviez') # Owner link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Harikushal') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+9YIHvEuJONc5MDBl') # Update channel link for the bot
 
 # ============================
