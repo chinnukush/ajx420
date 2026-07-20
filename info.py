2860d721907975b694ad4d25731c056ecb711f97
chinnukush <chinnukushal123@gmail.com>
2026-07-20 21:08:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ COVERX = bool(environ.get('COVERX', True)) # Use cover image for indexed files (
 # If you disable it then bot will use a default thumb for all files
 
 PICS_URL = (environ.get('PICS', 'https://api.aniwallpaper.workers.dev/random?type=girl')).split() #random anime girl img each time from aniwallpaper (Experimental)
-PICS = (environ.get('PICS', 'https://i.ibb.co/dsVtqQHG/d0c1d2b656d5.jpg')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://iili.io/CwXqUKJ.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
